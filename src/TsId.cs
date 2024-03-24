3cdf523a56fdd8cbff6730191f48424ac7ce4b71
Sepehr_Glpz <sepehrgolpazir@gmail.com>
2024-03-24 17:45:46 +0330

    Initial Commit

---
+++ src/TsId.cs
@@ -0,0 +1,96 @@
+﻿using SimpleBase;
+using System.Numerics;
+using System.Runtime.Serialization;
+
+namespace SGSX.Common.TsId;
+
+[Serializable]
+public readonly struct TsId : IComparable<ulong>, ISerializable, IEquatable<ulong>, IEqualityOperators<TsId, TsId, bool>
+{
+    #region Fields
+
+    private readonly ulong _value;
+
+    #endregion
+
+    #region Props
+    public DateTimeOffset DefaultCreationDate => TsIdGenerator.DefaultEpoch + TimeSpan.FromMilliseconds(_value >> (NODE_ID_BIT_SIZE + COUNTER_BIT_SIZE));
+    public DateTimeOffset CreationDate(DateTimeOffset epoch) => epoch + TimeSpan.FromMilliseconds(_value >> (NODE_ID_BIT_SIZE + COUNTER_BIT_SIZE));
+    public byte NodeId => (byte)((_value >> 16) & 0b_00111111);
+    public ushort Counter => (ushort)_value;
+
+    #endregion
+
+    #region Constructors
+
+    private TsId(ulong value) => _value = value;
+
+    public TsId(ulong timestamp, byte nodeId, ushort counter) =>
+        (_value) = Generate(SanatizeTimestamp(timestamp), SanatizeNodeId(nodeId), counter);
+
+    static TsId()
+    {
+        _factory = new TsIdGenerator();
+    }
+
+    #endregion
+
+    #region Static Methods
+    public static TsId NewTsId() => _factory.NewTsId();
+
+    public static TsId FromLong(long value) => new((ulong)value);
+    public static TsId FromLong(ulong value) => new(value);
+    public static TsId FromString(string base32Value) => Base32.Crockford.DecodeUInt64(base32Value);
+
+    public static implicit operator TsId(ulong value) => new(value);
+    public static implicit operator ulong(TsId value) => value._value;
+
+    public static bool operator ==(TsId left, TsId right) => left._value == right._value;
+    public static bool operator !=(TsId left, TsId right) => left._value != right._value;
+    public static bool operator <(TsId left, TsId right) => left._value < right._value;
+    public static bool operator <=(TsId left, TsId right) => left._value <= right._value;
+    public static bool operator >(TsId left, TsId right) => left._value > right._value;
+    public static bool operator >=(TsId left, TsId right) => left._value >= right._value;
+
+    #endregion
+
+    #region Private Utility
+    private static readonly TsIdGenerator _factory;
+
+    private static ulong SanatizeTimestamp(ulong value) => (value & 0b00000000_00000000_00000011_111111111_11111111_11111111_11111111_11111111);
+    private static byte SanatizeNodeId(byte nodeId) => (byte)(nodeId & 0b_00111111);
+
+    private const byte NODE_ID_BIT_SIZE = 6;
+    private const byte TIMESTAMP_BIT_SIZE = (sizeof(uint) + 10) * 8;
+    private const byte COUNTER_BIT_SIZE = sizeof(ushort) * 8;
+    private static ulong Generate(ulong timestamp, byte nodeId, ushort counter)
+    {
+        ulong id = 0;
+        id |= timestamp;
+        id <<= NODE_ID_BIT_SIZE;
+        id |= nodeId;
+        id <<= COUNTER_BIT_SIZE;
+        id |= counter;
+
+        return id;
+    }
+
+    #endregion
+
+    #region Impl
+
+    public int CompareTo(ulong other) => _value.CompareTo(other);
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context) => 
+        info.AddValue(nameof(_value), _value);
+
+    public bool Equals(ulong other) => _value == other;
+
+    public override bool Equals(object? obj) => _value.Equals(obj);
+
+    public override int GetHashCode() => _value.GetHashCode();
+
+    public override string ToString() => Base32.Crockford.Encode(BitConverter.GetBytes(_value));
+
+    #endregion
+}
---
+++ src/TsIdGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System.Security.Cryptography;
+using System.Threading;
+
+namespace SGSX.Common.TsId;
+public class TsIdGenerator
+{
+    #region Fields
+
+    private readonly byte _nodeId;
+
+    private volatile uint _counter;
+
+    private readonly DateTimeOffset _epoch;
+
+    public static readonly DateTimeOffset DefaultEpoch;
+
+    #endregion
+
+    #region Constructors
+    public TsIdGenerator(byte nodeId, DateTimeOffset epoch) : this(nodeId) => 
+        _epoch = epoch;
+
+    public TsIdGenerator(byte nodeId) => 
+        _nodeId = nodeId;
+
+    public TsIdGenerator() : base()
+    {
+        _nodeId = CreateDefaultNodeId();
+        _counter = 0;
+        _epoch = DefaultEpoch;
+    }
+
+    static TsIdGenerator()
+    {
+        DefaultEpoch = new DateTimeOffset(new DateOnly(2020, 1, 1), TimeOnly.MinValue, TimeSpan.Zero);
+    }
+    #endregion
+
+    #region Methods
+
+    public TsId NewTsId() => new(GetTimeStamp(), _nodeId, NextCount());
+
+    #endregion
+
+    #region Private Methods
+    private ushort NextCount() => (ushort)Interlocked.Increment(ref _counter);
+
+    private ulong GetTimeStamp() => Convert.ToUInt64((DateTimeOffset.UtcNow - _epoch).TotalMilliseconds);
+
+    private static byte CreateDefaultNodeId()
+    {
+        // 16 byte array of machine name
+        var hashedBytes =
+            MD5.HashData(Encoding.UTF8.GetBytes(Environment.MachineName)).AsSpan();
+
+        const byte MAX_NODE_SIZE = 0b00111111;
+
+        int ulongSize = sizeof(ulong);
+
+        // first 8 bytes
+        ulong first = BitConverter.ToUInt64(hashedBytes[0..ulongSize]);
+
+        // second 8 bytes
+        ulong second = BitConverter.ToUInt64(hashedBytes[ulongSize..(ulongSize * 2)]);
+
+        // shuffle bits into 8 bytes
+        ulong randomized = ~(first ^ second);
+
+        // control for 0 values
+        randomized = randomized switch
+        {
+            not 0 => randomized,
+            _ => 1, // fallback to 1
+        };
+
+        // get the number of digits in the number
+        var digits = Math.Floor(Math.Log10(randomized) + 1);
+
+        // move all the digits behind the dec point
+        decimal behindDec = (decimal)(randomized / Math.Pow(10, digits));
+
+        // multiply by max node value to get random number in the range
+        return (byte)Math.Floor(behindDec * MAX_NODE_SIZE);
+    }
+    #endregion
+}
